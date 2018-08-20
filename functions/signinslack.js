87aa18fb93a2be2df027f8a8344c99e7ddc71ae6
nakajmg <nakajima.jmg@gmail.com>
2018-08-20 20:07:31 +0900

    update function

--- functions/signinslack.js
+++ functions/signinslack.js
@@ -1,22 +1,24 @@
 const dotenv = require("dotenv").config()
-const env = dotenv.error ? null : dotenv.parsed
+const env = dotenv.error ? process.env : dotenv.parsed
 const axios = require("axios")
 const queryString = require("query-string")
 const url = "https://slack.com/api/oauth.access"
 exports.handler = ({ queryStringParameters: query, headers, path }, context, callback) => {
-  console.log(process.env)
   const code = query.code
   const appURL = query.state
-  const redirectURI = `http://${headers.host}/signinslack`
-  const client_id = env ? env.SLACK_CLIENT_ID : SLACK_CLIENT_ID
-  const client_secret = env ? env.SLACK_CLIENT_SECRET : SLACK_CLIENT_SECRET
+  console.log(path, headers)
+  const redirectURI =
+    process.env.NODE_ENV === "dev"
+      ? `http://${headers.host}/signinslack`
+      : "https://festive-ride-32b5bd.netlify.com/.netlify/functions/signinslack"
+  console.log(redirectURI)
   if (code) {
     return axios
       .get(url, {
         params: {
           code,
-          client_id,
-          client_secret,
+          client_id: env.SLACK_CLIENT_ID,
+          client_secret: env.SLACK_CLIENT_SECRET,
           redirect_uri: redirectURI,
         },
       })
